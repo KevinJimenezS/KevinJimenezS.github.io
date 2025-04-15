26676717bf6d5563a6f78a1eeee0959758a37fa4
KevinJimenezS <kevinjimenez343@aragon.unam.mx>
2025-04-15 01:56:28 -0600

    subiendo mi pagina

---
+++ assets/js/cliente.js
@@ -0,0 +1,22 @@
+/*function saludar(){
+    console.log("Diseño Web 1")
+    //alert("DIseño Web")
+    console.log(document);
+    console.log(document.getElementById("Parrafo1").innerHTML);
+    console.log(document.getElementById("Parrafo1").getAttribute("style"));    
+    
+}
+
+function cambiarParrafo2(){
+    document.getElementById('Parrafo 2').innerHTML= "FES ARAGON UNAM";
+   
+    
+}
+
+function cambiarFondoBody(){
+    const colores =["#FFFFFF", "#33FF57", "#2222FF", "#22FF11", "#FF2222"];
+    const colorALEA = colores[Math.floor(Math.random()* 5)];
+    console.log(colorALEA);
+    document.getElementById("cuerpo").style.backgroundColor=colorALEA;
+}
+*/
